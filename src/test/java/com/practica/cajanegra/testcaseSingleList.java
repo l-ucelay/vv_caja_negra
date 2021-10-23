80b9a9cdfc76397eedb22c1ef2a75ea982fc12ee
luis ucelay ulecia <luisucelay00@gmail.com>
2021-10-23 11:55:09 +0200

    Primer Commit

---
+++ src/test/java/com/practica/cajanegra/testcaseSingleList.java
@@ -0,0 +1,17 @@
+package com.practica.cajanegra;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class testcaseSingleList {
+
+	@Test
+	void test() {
+	
+		fail("Not yet implemented");
+	}
+
+}
